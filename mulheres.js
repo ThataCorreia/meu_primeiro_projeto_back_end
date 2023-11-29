ee58143221e2b6d63584fcde3c0a1ff88581bc5a
ThataCorreia <thaiscorreiab@gmail.com>
2023-11-28 22:02:02 -0300

    modified:   mulheres.js

--- mulheres.js
+++ mulheres.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {v4:uuidv4} = require('uuid')
-
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 app.use(express.json())
@@ -54,12 +53,73 @@ function mostraMulheres(request, response) {
 
 }
 
-function mostraPorta() {
+function corrigeMulher(request, response) {
 
-    console.log("Servidor criado e rodando na porta ", porta)
+    function encontraMulher(mulher) {
 
+        if (mulher.id === request.params.id) {
+
+            return mulher
+
+        }
+
+    }
+
+
+
+    const mulherEncontrada = mulheres.find(encontraMulher)
+
+
+
+    if (request.body.nome) {
+
+        mulherEncontrada.nome = request.body.nome
+
+    }
+
+
+
+    if (request.body.minibio) {
+
+        mulherEncontrada.minibio = request.body.minibio
+
+    }
+
+
+
+    if (request.body.imagem) {
+
+        mulherEncontrada.imagem = request.body.imagem
+
+    }
+
+    response.json(mulheres)
 }
+
+function deletaMulher(request, response) {
+
+    function todasMenosEla(mulher) {
+
+        if (mulher.id !== request.params.id) {
+
+            return mulher
+        }
+    }
+
+    const mulheresQueFicaram = mulheres.filter(todasMenosEla)
+
+    response.json(mulheresQueFicaram)
+}
+
 
 app.use(router.get('/mulheres', mostraMulheres))
 app.use(router.post('/mulheres', criaMulher))
+app.use(router.patch('/mulheres/:id', corrigeMulher))
+app.use(router.delete('/mulheres/:id', deletaMulher))
+
+function mostraPorta() {
+
+    console.log("Servidor criado e rodando na porta ", porta)
+}
+
 app.listen(porta, mostraPorta)
